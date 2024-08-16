352cf36256e15b7a8715ec81e67f5e1da71cea3a
SHUBHAM KUMAR DAS <137599709+SKD151105@users.noreply.github.com>
2024-08-16 17:28:49 +0530

    Add files via upload

---
+++ Habit Tracker/main.py
@@ -0,0 +1,62 @@
+import requests
+from datetime import datetime
+
+# The Graph : https://pixe.la/v1/users/shubhamkrdas/graphs/graph1
+
+USERNAME = "shubhamkrdas"
+TOKEN = "kdkfjldsfjsdjfdslflsd"
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Course Tracker",
+    "unit": "hours",
+    "type": "float",
+    "color": "shibafu",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),  # Formatted the date in required format and automated the process
+    "quantity": "2",
+}
+
+# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "3.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
